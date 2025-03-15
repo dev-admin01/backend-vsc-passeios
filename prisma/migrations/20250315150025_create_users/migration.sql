4ec8df9bbbbda51858d64aaafd86d149f9c715ce
bruno-lima1504 <bruno.lima1504@gmail.com>
2025-03-15 12:01:45 -0300

    feat: add migrations for create users

--- prisma/migrations/20250315150025_create_users/migration.sql
+++ prisma/migrations/20250315150025_create_users/migration.sql
@@ -1,9 +1,10 @@
 -- CreateTable
 CREATE TABLE "users" (
     "id_user" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" VARCHAR(256) NOT NULL,
     "email" VARCHAR(254) NOT NULL,
     "password" VARCHAR(72) NOT NULL,
+    "id_position" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -12,3 +13,6 @@ CREATE TABLE "users" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_id_position_fkey" FOREIGN KEY ("id_position") REFERENCES "positions"("id_position") ON DELETE RESTRICT ON UPDATE CASCADE;
